bbeed15107e2c31357af36c9e5eada010f50fa4f
ttreit <14933277+ttreit@users.noreply.github.com>
2019-06-16 14:48:22 -0700

    Add Question Number Functionality

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,30 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-   let allQuestions = QuestionBank()
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       let firstQuestion = allQuestions.list[0]
+       let firstQuestion = allQuestions.list[questionNumber]
         questionLabel.text = firstQuestion.questionText
-        
-        
-        
     }
-
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
   
     }
-    
     
     func updateUI() {
       
@@ -46,7 +47,12 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("Correct! You got it.")
+        } else {
+            print("Wrong!")
+        }
     }
     
     
